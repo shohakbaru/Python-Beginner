ddfe96abd5181aa5569971c1c0f27a1b9dbbd6f2
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-09-29 10:50:18 +0500

    some are updated

--- checkin.py
+++ checkin.py
@@ -4,4 +4,6 @@ mehmonlar = ['Ali', 'Vali', 'Hasan', 'Husan', 'Olim']
 
 #mehmonlar ro'yxatidagi har bir mehmon uchun uning ismini konsolga chiqaramiz
 for mehmon in mehmonlar:
-    print(mehmon)
+    print(f"Hurmatli {mehmon},")
+    print("Sizni 20-Mart kuni nahorgi oshga taklif qilamiz.")
+    print("Hurmat bilan, Usmonovlar oilasi.\n")
