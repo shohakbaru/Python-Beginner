546ab61b84a4b0309f7623fd7de6ad86edc77830
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-09-29 10:22:00 +0500

    Experience is finished

--- checkin.py
+++ checkin.py
@@ -1,28 +1,22 @@
-#120dan 1200gacha bo'lgan juft sonlat ro'yxati kerak
-juft_sonlar = list(range(120, 1200, 2)) #120dan 1200gacha 2 qadam bilan
-#konsolga chiqaramiz
-print(juft_sonlar)
-#joy tashlaymiz
-print("\n\n\n\n\n\n\n\n\n\n\n\n")
-#joft_sonlar ro'yxatidagi barcha sonlarni yig'indisini hisoblaymiz
-yigindi = sum(juft_sonlar)
-print(yigindi)
-#ro'yxatdagin eng katta sondan eng kichigini ayiramiz
-eng_kichik = min(juft_sonlar)
-eng_katta = max(juft_sonlar)
-ayirma = eng_katta - eng_kichik
-print(ayirma)
-#ro'yxatdagi elementlar sonini hisoblaymiz
-print(len(juft_sonlar))
-#boshidan o'rtasidan oxiridan 20ta element
-beginning_elements = juft_sonlar[:5]
+#taomlar degan ro'yxat yaratamiz
+taomlar = ['somsa', 'tandir', 'kabob', 'manti', 'palov']
 
-# Take 20 elements from the middle of the list
-middle_elements = juft_sonlar[len(juft_sonlar)//2 - 5:len(juft_sonlar)//2 + 5]
+#nonushta degan yangi listga taomlardan bir nechta element copy qilamiz
+nonushta = [taomlar[0], taomlar[-1]]
 
-# Take 20 elements from the end of the list
-end_elements = juft_sonlar[-5:]
+#konsolga
+print(nonushta)
 
-print("Beginning elements:", beginning_elements)
-print("Middle elements:", middle_elements)
-print("End elements:", end_elements)
+#nonushta ro'yxatida taomlar kamligi sabab yana 2ta element qo'shamiz
+nonushta.append("tuxum")  #>>>> tuxum elementi qo'shildi
+nonushta.append("kofe")  #>>>> nonushtalarga kofe qo'shildi
+
+#konsolga
+print(nonushta)
+print(taomlar)
+
+#nonushta ro'yxatini o'zgarmas ro'yxatga aylantiramiz
+nonushta = tuple(nonushta)
+
+#konsolga
+print(nonushta)
