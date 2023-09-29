16cfea88859c96e2c3988be95a29aea345195fdb
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-09-29 10:38:46 +0500

    Starting For Loop

--- checkin.py
+++ checkin.py
@@ -1,22 +1,7 @@
-#taomlar degan ro'yxat yaratamiz
-taomlar = ['somsa', 'tandir', 'kabob', 'manti', 'palov']
+#LEARNING FOR LOOP
+#mehmonlar degan ro'yxat yaratamiz
+mehmonlar = ['Ali', 'Vali', 'Hasan', 'Husan', 'Olim']
 
-#nonushta degan yangi listga taomlardan bir nechta element copy qilamiz
-nonushta = [taomlar[0], taomlar[-1]]
-
-#konsolga
-print(nonushta)
-
-#nonushta ro'yxatida taomlar kamligi sabab yana 2ta element qo'shamiz
-nonushta.append("tuxum")  #>>>> tuxum elementi qo'shildi
-nonushta.append("kofe")  #>>>> nonushtalarga kofe qo'shildi
-
-#konsolga
-print(nonushta)
-print(taomlar)
-
-#nonushta ro'yxatini o'zgarmas ro'yxatga aylantiramiz
-nonushta = tuple(nonushta)
-
-#konsolga
-print(nonushta)
+#mehmonlar ro'yxatidagi har bir mehmon uchun uning ismini konsolga chiqaramiz
+for mehmon in mehmonlar:
+    print(mehmon)
