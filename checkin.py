ae171f601bee4fce6ae5bb02df8cc95d4dc7cda5
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-09-29 09:20:33 +0500

    Tuples experience

--- checkin.py
+++ checkin.py
@@ -1,12 +1,25 @@
-#o'zgarmas ro'yxat yaratamiz
-toys = ('bus', 'car', 'bear', 'dino', 'snake', 'lizard')
-#o'zgarmas ro'yxatni oddiy ro'yxatga o'tkizamiz
-toys = list(toys)
-#Ro'yxatga o'zgartirish kiritamiz
-toys.append('dragon')
-toys.remove('bus')
-toys[1] = 'mcqueen'
-#Ro'yxatni yana o'zgarmas ro'yxatga aylantiramiz
-toys = tuple(toys)
-#konsolga chiqaramiz
-print(toys)
+#o'zgarmas ro'yhat yaratamiz
+davlatlar = ('Iroq', 'Malaysiya', 'Xitoy', 'Amerika', 'Norvegiya', 'O\'zbekiston')
+#ro'yhat uzunligini konsolga chiqaramiz
+print(len(davlatlar))
+#ro'yhatni tartiblaymiz
+#sorted() funksiyasi yordamida tartiblab konsolga chiqaramiz
+davlatlar2 = davlatlar[:]
+print(sorted(davlatlar2))
+#asl ro'yxatni konsolga chiqaramiz
+print(davlatlar)
+#ro'yhatni reverse() bilan chiqaramiz
+davlatlar3 = davlatlar[:]
+davlatlar3 = list(davlatlar3)
+davlatlar3.reverse()
+print(davlatlar3)
+#sort() yordamida ro'yhatni copy qilib keyin alifbo bo'yicha chiqaramiz
+davlatlar4 = davlatlar[:]
+davlatlar4 = list(davlatlar4)
+davlatlar4.sort()
+print(davlatlar4)
+#sort() yordamida ro'yxatni copy qilib alifboga teskari ravishda chiqaramiz
+davlatlar5 = davlatlar[:]
+davlatlar5 = list(davlatlar5)
+davlatlar5.sort(reverse=True)
+print(davlatlar5)
