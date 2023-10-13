b0fdea0608efaba50558af36dbc2a8c637c5a73c
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-13 19:38:13 +0500

    If-else-06

--- checkin.py
+++ checkin.py
@@ -7,6 +7,11 @@
 #     else:
 #         print(avto.title())
 
-javob = float(input("12x6 nechchiga teng?>>>> "))
-if javob != 72:
-    print('Javob xato!')
+# javob = float(input("12x6 nechchiga teng?>>>> "))
+# if javob != 72:
+#     print('Javob xato!')
+
+yil = int(input("Tug'ilgan yilingizni kiriting: "))
+if 2023-yil<18:
+    print(f"Yoshingiz {2023-yil}da ekan.")
+    print("Kirish mumkin emas!")
