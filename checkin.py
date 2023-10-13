088d9288aa98d7f472e74516ccbadc1cee574822
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-13 19:44:59 +0500

    If-else-07

--- checkin.py
+++ checkin.py
@@ -17,3 +17,7 @@
 #     print("Kirish mumkin emas!")
 # else:
 #     print("Xush kelibsiz!")
+
+login = input("Yangi login tanlang: ")
+if len(login)>5: #login uzunligini tekshiramiz
+    print("Login 5 harfdan ko'proq bo'lishi lozim.")
