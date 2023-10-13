fe5829dfdadb082f51b2f698c253c1470cc158ff
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-13 19:33:45 +0500

    If-else-05

--- checkin.py
+++ checkin.py
@@ -1,8 +1,12 @@
 #If-else operatori Amaliyot
 
-avtolar = ['audi', 'bmw', 'volvo', 'kia', 'hyundai']
-for avto in avtolar:
-    if avtolar == 'bmw':
-        print(avto.upper())
-    else:
-        print(avto.title())
+# avtolar = ['audi', 'bmw', 'volvo', 'kia', 'hyundai']
+# for avto in avtolar:
+#     if avtolar == 'bmw':
+#         print(avto.upper())
+#     else:
+#         print(avto.title())
+
+javob = float(input("12x6 nechchiga teng?>>>> "))
+if javob != 72:
+    print('Javob xato!')
