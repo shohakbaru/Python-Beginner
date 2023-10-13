4106a1d537d3414fa628f2fa3f5bf43aa7081d3e
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-13 19:17:55 +0500

    If-else-02

--- checkin.py
+++ checkin.py
@@ -1,5 +1,11 @@
 #If-else operatori
 
-son = int(input("Istalgan son kiriting: "))
-if son>0:
-    print(son, "musbat son")
+# son = int(input("Istalgan son kiriting: "))
+# if son>0:
+#     print(son, "musbat son")
+
+yosh = int(input("Yoshingizni kiriting: "))
+if yosh<=7: #agar yosh 7dan kichik yoki teng bo'lsa
+    print("Sizga avtobus bepul")
+else: # aks holda ya'ni, yosh>7
+    print("Chipta narxi 5000 so'm")
