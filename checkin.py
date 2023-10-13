0cc2286bf4c6e68a7adad4c9f3db5be112c81806
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-13 19:29:18 +0500

    If-else-04

--- checkin.py
+++ checkin.py
@@ -1,14 +1,8 @@
-#If-else operatori
+#If-else operatori Amaliyot
 
-# son = int(input("Istalgan son kiriting: "))
-# if son>0:
-#     print(son, "musbat son")
-
-# yosh = int(input("Yoshingizni kiriting: "))
-# if yosh<=7: #agar yosh 7dan kichik yoki teng bo'lsa
-#     print("Sizga avtobus bepul")
-# else: # aks holda ya'ni, yosh>7
-#     print("Chipta narxi 5000 so'm")
-
-x, y = 25, 50 # x=25; y=50
-print("x>y") if x>y else print("x<y")
+avtolar = ['audi', 'bmw', 'volvo', 'kia', 'hyundai']
+for avto in avtolar:
+    if avtolar == 'bmw':
+        print(avto.upper())
+    else:
+        print(avto.title())
