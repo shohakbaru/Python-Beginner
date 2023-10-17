b60a1e42a13c064bfd5a9f1fdd3948df12ee09e6
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-17 10:29:58 +0500

    If-else-1.6

--- checkin.py
+++ checkin.py
@@ -49,8 +49,16 @@
 # else:
 #     print("Rahmat!")
 
+# son_1 = input("Birinchi sonni kiriting: ")
+# if int(son_1) >= 0:
+#     print("Musbat son!")
+# else:
+#     print("Manfiy son!")
+import math
+
 son_1 = input("Birinchi sonni kiriting: ")
-if int(son_1) >= 0:
-    print("Musbat son!")
+if int(son_1) > 0:
+    x = math.sqrt(int(son_1))
+    print(x)
 else:
-    print("Manfiy son!")
+    print("Musbat son kiriting!")
