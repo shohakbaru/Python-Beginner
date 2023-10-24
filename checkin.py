571b756ff3322aa1d6b4f338bae6b641071939e9
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-24 11:20:18 +0500

    Amaliyot 1.1

--- checkin.py
+++ checkin.py
@@ -13,15 +13,31 @@
 # else:
 #     print("Siz uchun chipta narxi 20.000 so'm.")
 
-a = input("Siz ikkita son kiritishingiz kerak. Bu yerga ularning birinchisini kiriting >>> ")
-b = input("Siz ikkita son kiritishingiz kerak. Bu yerga ularning ikkinchisini kiriting >>> ")
+# a = input("Siz ikkita son kiritishingiz kerak. Bu yerga ularning birinchisini kiriting >>> ")
+# b = input("Siz ikkita son kiritishingiz kerak. Bu yerga ularning ikkinchisini kiriting >>> ")
 
-a = int(a)
-b = int(b)
+# a = int(a)
+# b = int(b)
 
-if a==b:
-    print("Sonlar teng.")
-elif a>b:
-    print("Birinchi son katta.")
+# if a==b:
+#     print("Sonlar teng.")
+# elif a>b:
+#     print("Birinchi son katta.")
+# else:
+#     print("Ikkinchi son katta.")
+
+mahsulotlar = ['sovun', 'un', 'yog\'', 'kolbasa', 'pomidor', 'sabzi', 'kashnich', 'robot', 'futbolka', 'shim']
+savat = []
+
+savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Birinchisi >> ".lower()))
+savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Ikkinchisi >> ".lower()))
+savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Uchinchisi >> ".lower()))
+
+if savat:
+    for mahsulot in savat:
+        if mahsulot in mahsulotlar:
+            print(f"Bizda {mahsulot} mahsuloti bor.")
+        else:
+            print(f"Bizda {mahsulot} mahsuloti qolmagan.")
 else:
-    print("Ikkinchi son katta.")
+    print("Savatni to'ldiring!")
