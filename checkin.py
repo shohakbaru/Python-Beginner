cb2c4634e5b170eccb116a1bc8539bcf5f8c4052
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-24 12:19:00 +0500

    Amaliyot 2.0

--- checkin.py
+++ checkin.py
@@ -26,18 +26,51 @@
 # else:
 #     print("Ikkinchi son katta.")
 
-mahsulotlar = ['sovun', 'un', 'yog\'', 'kolbasa', 'pomidor', 'sabzi', 'kashnich', 'robot', 'futbolka', 'shim']
+# mahsulotlar = ['sovun', 'un', 'yog\'', 'kolbasa', 'pomidor', 'sabzi', 'kashnich', 'robot', 'futbolka', 'shim']
+# savat = []
+
+# savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Birinchisi >> ".lower()))
+# savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Ikkinchisi >> ".lower()))
+# savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Uchinchisi >> ".lower()))
+
+# if savat:
+#     for mahsulot in savat:
+#         if mahsulot in mahsulotlar:
+#             print(f"Bizda {mahsulot} mahsuloti bor.")
+#         else:
+#             print(f"Bizda {mahsulot} mahsuloti qolmagan.")
+# else:
+#     print("Savatni to'ldiring!")
+
+mahsulotlar = [
+    "un",
+    "yog'",
+    "sovun",
+    "tuxum",
+    "piyoz",
+    "kartoshka",
+    "olma",
+    "banan",
+    "uzum",
+    "qovun",
+]
+
+
 savat = []
+for n in range(5):
+    savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
 
-savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Birinchisi >> ".lower()))
-savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Ikkinchisi >> ".lower()))
-savat.append(input("Sotib olishni xohlagan 3ta mahsulotingiz nomini kiriting. Uchinchisi >> ".lower()))
+bor_mahsulotlar = []
+mavjud_emas = []
+for mahsulot in savat:
+    if mahsulot in mahsulotlar:
+        bor_mahsulotlar.append(mahsulot)
+    else:
+        mavjud_emas.append(mahsulot)
 
-if savat:
-    for mahsulot in savat:
-        if mahsulot in mahsulotlar:
-            print(f"Bizda {mahsulot} mahsuloti bor.")
-        else:
-            print(f"Bizda {mahsulot} mahsuloti qolmagan.")
+if mavjud_emas:
+    print("Do'konimizda quyidagi mahsulotlar yo'q:")
+    for mahsulot in mavjud_emas:
+        print(mahsulot)
 else:
-    print("Savatni to'ldiring!")
+    print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
