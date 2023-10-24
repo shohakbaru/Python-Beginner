3e08ff2a3b43d2e0a8db55438537f4be08ac9bb7
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-24 12:22:39 +0500

    Amaliyot 3.0

--- checkin.py
+++ checkin.py
@@ -42,35 +42,51 @@
 # else:
 #     print("Savatni to'ldiring!")
 
-mahsulotlar = [
-    "un",
-    "yog'",
-    "sovun",
-    "tuxum",
-    "piyoz",
-    "kartoshka",
-    "olma",
-    "banan",
-    "uzum",
-    "qovun",
+# mahsulotlar = [
+#     "un",
+#     "yog'",
+#     "sovun",
+#     "tuxum",
+#     "piyoz",
+#     "kartoshka",
+#     "olma",
+#     "banan",
+#     "uzum",
+#     "qovun",
+# ]
+
+
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulot)
+#     else:
+#         mavjud_emas.append(mahsulot)
+
+# if mavjud_emas:
+#     print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#     for mahsulot in mavjud_emas:
+#         print(mahsulot)
+# else:
+#     print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+
+
+foydalanuvchilar = [
+    "ahmat",
+    "admin",
+    "user",
+    "shoh",
+    "ahmad"
 ]
 
+login = input("Yangi login kiriting: ")
 
-savat = []
-for n in range(5):
-    savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
-
-bor_mahsulotlar = []
-mavjud_emas = []
-for mahsulot in savat:
-    if mahsulot in mahsulotlar:
-        bor_mahsulotlar.append(mahsulot)
-    else:
-        mavjud_emas.append(mahsulot)
-
-if mavjud_emas:
-    print("Do'konimizda quyidagi mahsulotlar yo'q:")
-    for mahsulot in mavjud_emas:
-        print(mahsulot)
+if login.lower() in foydalanuvchilar:
+    print("Login band, yangi login tanglang!")
 else:
-    print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+    print("Xush kelibsiz!")
