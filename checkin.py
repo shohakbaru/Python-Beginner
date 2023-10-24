19d599e95434023ba00a6d4463855c86be83625b
Shohakbar Usmonov <shohakbaru@gmail.com>
2023-10-24 11:08:07 +0500

    Amaliyot 0.2

--- checkin.py
+++ checkin.py
@@ -1,5 +1,14 @@
-a = input('Iltimos, juft son kiriting---> ')
-if int(a)%2!=0:
-    print("Kiritgan soningiz juft emas!")
+# a = input('Iltimos, juft son kiriting---> ')
+# if int(a)%2!=0:
+#     print("Kiritgan soningiz juft emas!")
+# else:
+#     print("Rahmat!")
+
+yosh = input("Xurmatli foydalanuvchi, yoshingizni kiriting >>> ")
+yosh = int(yosh)
+if yosh<4 or yosh>60:
+    print("Chipta siz uchun bepul!")
+elif yosh<18:
+    print("Siz uchun chipta narxi 10.000 so'm.")
 else:
-    print("Rahmat!")
+    print("Siz uchun chipta narxi 20.000 so'm.")
